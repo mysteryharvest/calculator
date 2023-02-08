e0f97328e8185187272256db1a2c99e90a789216
Garrett Kelly <boontdustie@users.noreply.github.com>
2023-02-07 17:28:33 -0800

    Update sketch.js
    Fixes double letters, and letters that are next to each other that have unique values

--- sketch.js
+++ sketch.js
@@ -1,15 +1,15 @@
-let input;
+let val;
 let resultP;
 let submitButton;
 
 function setup() {
   createCanvas(400, 400);
 
-  input = createInput();
-  input.position(20, 65);
+  val = createInput();
+  val.position(20, 65);
 
   submitButton = createButton('Submit');
-  submitButton.position(input.x + input.width, 65);
+  submitButton.position(val.x + val.width, 65);
   submitButton.mousePressed(calculate);
 
   resultP = createP('');
@@ -17,22 +17,49 @@ function setup() {
 }
 
 function calculate() {
-  let word = input.value();
+  let word = val.value();
   word = word.toUpperCase();
   let result = 0;
+  
   for (let i = 0; i < word.length; i++) {
+    console.log("index i:" + i);
+    console.log("char: " + word[i]);
+    console.log("result: " + result);
+    
+
+    // if second of a double letter (OO, TT, MM, DD, etc),
+    // just skip.
+    if (word[i] == word[i-1] && word[i] != 'O' && word[i] != 'E') {
+      break;
+    }
     switch (word[i]) {
       case 'A':
         result += 10;
         break;
       case 'E':
-        result += 20;
+        if (word[i - 1] == 'E') { // Preceding 'E' handled, skip
+          break;
+        }
+        else if (word[i + 1] == 'E') { // deal with double EE
+          result += 50;
+        }
+        else {
+          result += 20;
+        }
         break;
       case 'I':
         result += 30;
         break;
       case 'O':
-        result += 40;
+        if (word[i - 1] == 'O') { // Preceding 'O' handled, skip
+          break;
+        }
+        else if (word[i + 1] == 'O') { // deal with double OO
+          result += 60;
+        }
+        else {
+          result += 40;
+        }
         break;
       case 'U':
         result += 70;
@@ -47,14 +74,14 @@ function calculate() {
         result += 100;
         break;
       case 'H':
-        if (word[i - 1] == 'C' || word[i - 1] == 'S') {
-          result += 1;
+        if (word[i - 1] == 'C' || word[i - 1] == 'S'|| word[i - 1] == 'T' || word[i - 1] == 'P') {
+          break; // Let the s, c, t, p deal with it.
         } else {
           result += 5;
         }
         break;
       case 'S':
-        if (word[i - 1] == 'H') {
+        if (word[i + 1] == 'H') {
           result += 2;
         } else {
           result += 3;
@@ -79,10 +106,11 @@ function calculate() {
         result += 1;
         break;
       case 'P':
-        result += 2;
-        break;
-      case 'T':
-        result += 3;
+        if (word[i + 1] == 'H') {
+          result += 6;
+        } else {
+          result += 2;
+        }
         break;
       case 'D':
         result += 4;
@@ -113,7 +141,7 @@ function calculate() {
         result += 8;
         break;
       case 'T':
-        if (word[i - 1] == 'H') {
+        if (word[i + 1] == 'H') {
           result += 9;
         } else {
           result += 3;

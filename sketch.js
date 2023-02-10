dd1b71d52ad9eee947f49d1da5a8c427e10a150d
Garrett Kelly <boontdustie@users.noreply.github.com>
2023-02-10 13:38:27 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -48,6 +48,7 @@ function runThroughCalculator(word, i) {
   switch (word[i]) {
       case 'A':
         result += 10;
+        break;
       case 'E':
         if (word[i - 1] == 'E') { // Preceding 'E' handled, skip
            // do nothing
@@ -58,8 +59,10 @@ function runThroughCalculator(word, i) {
         else {
           result += 20;
         }
+        break;
       case 'I':
         result += 30;
+        break;
       case 'O':
         if (word[i - 1] == 'O') { // Preceding 'O' handled, skip
          // do nothing 
@@ -70,72 +73,100 @@ function runThroughCalculator(word, i) {
         else {
           result += 40;
         }
+        break;
       case 'U':
-          result += 70;
+        result += 70;
+        break;
       case 'G':
         result += 80;
+        break;
       case 'M':
         result += 90;
+        break;
       case 'N':
         result += 100;
+        break;
       case 'H':
         if (word[i - 1] == 'C' || word[i - 1] == 'S'|| word[i - 1] == 'T' || word[i - 1] == 'P') {
            // Let the s, c, t, p deal with it.
-        } else {
+        } 
+        else {
           result += 5;
         }
+        break;
       case 'S':
         if (word[i + 1] == 'H') {
           result += 2;
-        } else {
+        } 
+        else {
           result += 3;
         }
+        break;
       case 'Z':
         result += 4;
+        break;
       case 'F':
         result += 6;
+        break;  
       case 'R':
         result += 7;
+        break;
       case 'Y':
         result += 8;
+        break;
       case 'J':
         result += 9;
+        break;  
       case 'B':
         result += 1;
+        break;
       case 'P':
         if (word[i + 1] == 'H') {
           result += 6;
-        } else {
+        } 
+        else {
           result += 2;
         }
+        break;
       case 'D':
         result += 4;
+        break;
       case 'K':
         result += 5;
+        break;
       case 'L':
         result += 6;
+        break;   
       case 'W':
         result += 7;
+        break;  
       case 'V':
         result += 8;
+        break;     
       case 'C':
         if (word[i + 1] == 'H') {
           result += 1;
-        } else {
+        } 
+        else {
           result += 5;
         }
+        break;
       case 'Q':
         result += 12;
+        break;  
       case 'X':
         result += 8;
+        break;
       case 'T':
         if (word[i + 1] == 'H') {
           result += 9;
-        } else {
+        } 
+        else {
           result += 3;
         }
-      }
-      return result;
+        break; 
+   }
+   return result;
 }
 
  
